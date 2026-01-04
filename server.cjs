4b1d545e9ce6a424dbf8a0efa0475a2a3242d934
Thiago Falcao <thiagopedreiro28@gmail.com>
2026-01-04 00:10:37 -0300

    ainda

--- server.cjs
+++ server.cjs
@@ -376,7 +376,7 @@ async function monitorarExpiracoes() {
     }
 
     // 2. Limpa motoboys que deixaram o tempo de 60s acabar
-    /*const expirados = await pool.query(`
+    const expirados = await pool.query(`
       DELETE FROM exposicao_corrida 
       WHERE EXTRACT(EPOCH FROM (NOW() - data_exposicao)) >= 60
       RETURNING corrida_id
@@ -388,7 +388,7 @@ async function monitorarExpiracoes() {
        if (dados.rows.length > 0) {
          await distribuirCorridaParaMotoboys(exp.corrida_id, dados.rows[0].tipo_servico);
        }
-    }*/
+    }
 
   } catch (err) {
     console.error("Erro no monitor:", err);
@@ -950,7 +950,7 @@ app.post('/motoboy/status-online', async (req, res) => {
         await pool.query(`
           UPDATE usuarios SET
             online = true,  -- <--- FORÇA O TRUE
-            online_ate = NOW() + interval '15 seconds', -- 15s de tolerância
+            online_ate = NOW() + interval '120 seconds', -- 15s de tolerância
             latitude = $2,
             longitude = $3
           WHERE id = $1
